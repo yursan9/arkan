678671fb039c26c3a804dd8f0c5a2befda1b199b
Yurizal Susanto <yursan9@pm.me>
2019-09-19 04:18:19 +0700

    Initial implementation of overview
    We already have list sidebar and the overview,
    need to ensure everything is work ok, before consuming
    prayer time API

--- src/widgets/shalatoverview.py
+++ src/widgets/shalatoverview.py
@@ -1,6 +1,6 @@
-# window.py
+# shalatoverview.py
 #
-# Copyright 2019 Yurizal Susanto
+# Copyright 2019 Yurizal Susanto <yursan9@pm.me>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -14,31 +14,19 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
 
 import gi
 gi.require_version('Gtk', '3.0')
-gi.require_version('Handy', '0.0')
 
-from gi.repository import Gtk, Handy
-Handy.init()
+from gi.repository import Gtk
 
-from .widgets.shalatlist import ShalatList
-from .widgets.shalatoverview import ShalatOverview
 
-@Gtk.Template(resource_path='/com/github/yursan9/Arkan/ui/window.ui')
-class Window(Gtk.ApplicationWindow):
-    __gtype_name__ = 'Window'
-
-    contents = Gtk.Template.Child()
+@Gtk.Template(resource_path='/com/github/yursan9/Arkan/ui/shalatoverview.ui')
+class ShalatOverview(Gtk.Box):
+    __gtype_name__ = 'ShalatOverview'
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-
-        shalat_list = ShalatList()
-        shalat_list.populate()
-        self.contents.add(shalat_list)
-
-        shalat_overview = ShalatOverview()
-        self.contents.add(shalat_overview)
-
-        self.show_all()
+        
